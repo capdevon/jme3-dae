38129ce2dd37c10309cd087d0f95f77edf5c9bbd
Valkyrie <valkyrie@nasa.gov>
2022-03-15 20:13:32 -0500

    Initial import.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+   id("us.ihmc.ihmc-build")
+   id("us.ihmc.ihmc-ci") version "7.6"
+   id("us.ihmc.ihmc-cd") version "1.23"
+   id("us.ihmc.log-tools-plugin") version "0.6.3"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.0"
+   vcsUrl = "https://stash.ihmc.us/"
+   openSource = false
+   
+   configureDependencyResolution()
+   configurePublications()
+}
+
+val jmeVersion = "3.5.0-stable"
+
+mainDependencies {
+   api("org.jmonkeyengine:jme3-core:$jmeVersion")
+   api("org.jmonkeyengine:jme3-desktop:$jmeVersion")
+}
